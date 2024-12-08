bc51d7309a9c4de908b210bc5f2839a746984733
Santi Miranda <santimir@protonmail.com>
2024-12-08 11:51:33 +0000

    refactor: get best path from regex.

--- src/torch_practice/utils/io.py
+++ src/torch_practice/utils/io.py
@@ -4,6 +4,7 @@ Pass the whole "config" makes it more robust.
 """
 
 import logging
+import re
 from datetime import datetime, timezone
 from pathlib import Path
 from typing import NamedTuple
@@ -20,28 +21,33 @@ logger = logging.getLogger(__package__)
 def get_best_path(dirname: Path, mode: LossModeType) -> Path:
   """Find best model if filename is not specified by user.
 
-  Parses the filenames and gets best name from best loss.
+  The function uses a regex/pattern to find a float in the filename.
+  If it doesn't, it throws a "LossNotFoundError."
   """
-  best_loss = float("-inf") if mode == "max" else float("inf")
-  best_name = Path()
+  # match files like `abc_0.124.pth` (or `.pt`)
+  pattern = r".*_(\d+\.\d+)\.pth?$"
+  best_loss = None  # None is taken care of within loss_improved.
+  best_name = None
 
-  path_names = dirname.glob("*.pth")  # may be empty raising error later on.
-  try:  # so we "try"
-    for pathname in path_names:
-      loss = float(pathname.stem.split("_")[-1])
-      improved = loss_improved(best_loss, loss, mode)
-      if improved:
-        best_loss = loss
-        best_name = pathname
+  for file in dirname.iterdir():
+    if file.is_file():
+      match = re.match(pattern, file.name)
+      logger.debug("Found model file.")
+      if match:
+        msg = f"Found match from regex ({match.group(1)})."
+        logger.debug(msg)
+        loss = float(match.group(1))
+        improved = loss_improved(best_loss, loss, mode)
+        if improved:
+          best_loss = loss
+          best_name = file
 
-  except StopIteration as sti:
-    msg = "No model matches the pattern '*.pth'"
-    raise FileNotFoundError(msg) from sti
-
-  if best_name.name == "":
-    msg = "Could not parse loss from filename."
-    raise LossesNotFoundError(msg)
-  return dirname / best_name
+  if best_name is None:
+    msg = f"Could not parse loss from filenames in dir: {dirname}."
+    raise LossNotFoundError(msg)
+  msg = f"Best filename-model found {best_name}"
+  logger.debug(msg)
+  return best_name  # full path
 
 
 def load_model(
@@ -100,5 +106,5 @@ def make_savedir(basedir: str) -> Path:
   return savedir
 
 
-class LossesNotFoundError(Exception):
+class LossNotFoundError(Exception):
   pass
--- src/torch_practice/utils/io_test.py
+++ src/torch_practice/utils/io_test.py
@@ -1,38 +1,47 @@
 """Test io operations using tmp_path."""
 
+import pytest
 import torch
 
 from torch_practice.default_config import default_config
 from torch_practice.nn_arch import DynamicAE
 from torch_practice.utils.io import (
+  LossNotFoundError,
   get_best_path,
   load_model,
   make_savedir,
 )
 
 
-def test_get_best_path(tmp_path):
-  """Test if a .pth file is removed.
+class TestGetBestPath:
+  def test_best_path(self, tmp_path):
+    """Test if a .pth file is removed.
 
-  Explicitly make a temporary directory rather than using
-  `tmp_path` fixture.
-  """
-  losses = [0.5, 0.6, 0.3, 0.01, 0.55]
-  for epoch, loss in enumerate(losses):
-    # create "models" in the path.
-    file = tmp_path / f"{epoch}_{loss}.pth"
-    file.write_bytes(b"hello world")
+    Explicitly make a temporary directory rather than using
+    `tmp_path` fixture.
+    """
+    losses = [0.5, 0.6, 0.3, 0.01, 0.55]
+    for epoch, loss in enumerate(losses):
+      # create "models" in the path.
+      file = tmp_path / f"{epoch}_{loss}.pth"
+      file.write_bytes(b"hello world")
 
-  # make sure we use the temporary path
-  dirname = tmp_path.resolve()
+    # make sure we use the temporary path
+    dirname = tmp_path.resolve()
 
-  # test min
-  path = get_best_path(dirname, "min")
-  assert path.stem.split("_")[-1] == "0.01"
+    # test min
+    path = get_best_path(dirname, "min")
+    assert path.stem.split("_")[-1] == "0.01"
 
-  # test max
-  path = get_best_path(dirname, "max")
-  assert path.stem.split("_")[-1] == "0.6"
+    # test max
+    path = get_best_path(dirname, "max")
+    assert path.stem.split("_")[-1] == "0.6"
+
+  def test_error_no_file(self, tmp_path):
+    """When directory exists, but file doesn't it must error."""
+    # tmp_path is unique for each fn invokation
+    with pytest.raises(LossNotFoundError):
+      get_best_path(tmp_path.resolve(), "min")
 
 
 def test_save_load(tmp_path):
